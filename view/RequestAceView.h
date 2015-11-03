3484266cf71382acba32b2e566d5825829cad66f
Toby Harris <operator@tobyz.net>
2015-11-03 18:24:59 +0000

    ACEView subclass with hardcoded <NowPlus5s> token replacement

---
+++ view/RequestAceView.h
@@ -0,0 +1,14 @@
+//
+//  RequestTextView.h
+//  CocoaRestClient
+//
+//  Created by Toby Harris on 03/11/2015.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ACEView/ACEView.h"
+
+@interface RequestACEView : ACEView
+
+@end
