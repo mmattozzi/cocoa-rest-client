c054a9b1fc1ef9d634e2b314bc83be82c341628b
Diego Massanti <diego@massanti.com>
2016-03-09 04:28:11 -0300

    Removed a LOT of UI glue code and replaced it with equivalent cocoa bindings (lots of code deleted on app delegate) Replaced local defines with global FOUNDATION_EXPORT constants now available in CRCConstants.h/m Added persistence for "Syntax Highlight" and "Raw Input". Removed IBActions and setters for now obsolete raw request / highlight from app delegate. Added defaults.plist with some sane defaults.

---
+++ CRCconstants.m
@@ -0,0 +1,24 @@
+//
+//  CRCconstants.m
+//  
+//
+//  Created by Diego Massanti on 3/9/16.
+//
+//
+
+#import "CRCconstants.h"
+
+@implementation CRCconstants
+
+NSString * const FOLLOW_REDIRECTS = @"followRedirects";
+NSString * const APPLY_HTTP_METHOD_ON_REDIRECT = @"applyHttpMethodOnRedirect";
+NSString * const SYNTAX_HIGHLIGHT = @"UIMenuSyntaxHighlight";
+NSString * const RAW_REQUEST_BODY = @"UIRawRequestBody";
+NSString * const RESPONSE_TIMEOUT = @"responseTimeout";
+NSString * const SAVED_DRAWER_SIZE = @"savedDrawerSize";
+NSString * const THEME = @"theme";
+NSUInteger const DEFAULT_FONT_SIZE = 12;
+NSString * const SHOW_LINE_NUMBERS = @"showLineNumbers";
+
+
+@end
